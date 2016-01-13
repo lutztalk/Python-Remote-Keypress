b2e00011aa75f2dfe8c5930b3ca34349e53c7c1a
AustinLutz <austingrierlutz@gmail.com>
2016-01-13 15:31:05 -0500

    Fixing some errors

---
+++ 8841-KeyPress-Template.py
@@ -0,0 +1,44 @@
+from lxml import etree
+from lxml.etree import tostring
+from copy import deepcopy
+import requests
+import time
+from StringIO import StringIO
+_79xx_keynavlist = ['Key:Applications','Key:KeyPad5','Key:KeyPad4','Key:KeyPad4','Key:Soft3']
+key = 'XML'
+keynav = {key : []}
+for keypress in _79xx_keynavlist:
+    ph_nav = etree.Element('CiscoIPPhoneExecute')
+    exeit_e = etree.SubElement(ph_nav, 'ExecuteItem')
+    exeit_e.set('Priority','0')
+    exeit_e.set('URL',keypress)
+    phnavstr = etree.tostring(ph_nav,pretty_print=True)
+    print phnavstr
+    keynav[key] = keynav[key]+[phnavstr]
+
+key_action = deepcopy(keynav)
+
+url = 'http://192.168.5.197/CGI/Execute'
+user='phonecontrol'
+pwd='phonecontrol'
+headers={'content-type':'application/xml'}
+counter = 0
+for xml in keynav[key]:
+    key_press = {}
+    key_press[key] = xml
+    r = requests.post(url,headers=headers,data=key_press,auth=(user,pwd))
+    key_action[key].remove(xml)
+    if(counter == 4):
+        break
+    counter += 1
+for i in range(1,3):
+    counter = 1
+    for action in key_action[key]:
+        keyact = {}
+        keyact[key] = action
+        r = requests.post(url,headers=headers,data=keyact,auth=(user,pwd))
+        if counter != 3:
+            time.sleep(50.0/1000.0)
+        else:
+            time.sleep(2)
+        counter += 1
---
+++ README.md
@@ -0,0 +1,9 @@
+# 8841 Cisco IP phone 
+A quick and simple python script I took from samiamsam.com and tweaked to work with 8841 model Cisco IP phones.
+
+http://samiamsam.com/2014/04/16/cisco-uc-devops-series-remotely-deleting-the-itl-cert-from-a-7900-series-phone/
+
+Instructions
+
+-On the CUCM your devices are registered to, create an application user with the "Standard CTI Enabled" Permissions. Once created, assign all devices you are wanting to control to its list of "Controlled Devices".
+
