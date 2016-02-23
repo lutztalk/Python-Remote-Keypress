aea6b4894e86b22b813ce1032516560007616a55
AustinLutz <austingrierlutz@gmail.com>
2016-02-23 09:27:56 -0500

    Added support for CSV

--- 8841-KeyPress-Template.py
+++ 8841-KeyPress-Template.py
@@ -9,7 +9,7 @@ import time
 import csv
 
 def buttonpress(ip):
-  _88xx_keynavlist = ['Key:Applications','Key:KeyPad5','Key:KeyPad4','Key:KeyPad4','Key:Soft3','Key:Soft1']
+  _88xx_keynavlist = ['Key:Applications','Key:KeyPad6','Key:KeyPad4','Key:KeyPad4','Key:Soft3','Key:Soft1']
   key = 'XML'
   keynav = {key : []}
   for keypress in _88xx_keynavlist:
@@ -20,26 +20,36 @@ def buttonpress(ip):
       phnavstr = etree.tostring(ph_nav,pretty_print=True)
       print phnavstr
       keynav[key] = keynav[key]+[phnavstr]
+
   key_action = deepcopy(keynav)
+
   url = 'http://'+ip+'/CGI/Execute'
   user='phonecontrol'
   pwd='phonecontrol'
   headers={'content-type':'application/xml'}
   counter = 0
   for xml in keynav[key]:
-      key_press = {}
-      key_press[key] = xml
-      r = requests.post(url,headers=headers,data=key_press,auth=(user,pwd))
-      key_action[key].remove(xml)
-      time.sleep(1.0)
-      if(counter == 4):
-          break
-      counter += 2000
+    key_press = {}
+    key_press[key] = xml
+    r = requests.post(url,headers=headers,data=key_press,auth=(user,pwd))
+    key_action[key].remove(xml)
+    time.sleep(1.0)
+    if(counter == 4):
+        break
+    counter += 2000
   for i in range(1,2000):
-      counter = 2000
-      for action in key_action[key]:
-          keyact = {}
-          keyact[key] = action
-          r = requests.post(url,headers=headers,data=keyact,auth=(user,pwd))
-buttonpress("192.168.5.185")
-buttonpress("192.168.5.186")
+    counter = 2000
+    for action in key_action[key]:
+        keyact = {}
+        keyact[key] = action
+        r = requests.post(url,headers=headers,data=keyact,auth=(user,pwd))
+
+print "Starting ITL Delete"
+
+with open('phoneiplist.csv','rb') as f:
+  reader = csv.reader(f)
+  for row in reader:
+    phoneIP = '. '.join(row)
+    buttonpress(phoneIP)
+
+print "All Done!!"
