922ef72af14b90b6d23fa8387b6c73592aee4edd
AustinLutz <austingrierlutz@gmail.com>
2016-02-22 19:18:33 -0500

    defined function and called function a million times

--- 8841-KeyPress-Template.py
+++ 8841-KeyPress-Template.py
@@ -1,48 +1,45 @@
 from lxml import etree
 from lxml.etree import tostring
 from copy import deepcopy
+from StringIO import StringIO
 import requests
 import time
 import sys
 import time
-from StringIO import StringIO
-_79xx_keynavlist = ['Key:Applications','Key:KeyPad2','Key:KeyPad1','Key:NavRight','Key:Soft2','Key:Soft1']
+import csv
 
-
-key = 'XML'
-keynav = {key : []}
-for keypress in _79xx_keynavlist:
-    ph_nav = etree.Element('CiscoIPPhoneExecute')
-    exeit_e = etree.SubElement(ph_nav, 'ExecuteItem')
-    exeit_e.set('Priority','0')
-    exeit_e.set('URL',keypress)
-    phnavstr = etree.tostring(ph_nav,pretty_print=True)
-    print phnavstr
-    keynav[key] = keynav[key]+[phnavstr]
-
-key_action = deepcopy(keynav)
-url = 'http://192.168.5.222/CGI/Execute'
-user='phonecontrol'
-pwd='phonecontrol'
-headers={'content-type':'application/xml'}
-counter = 0
-for xml in keynav[key]:
-    key_press = {}
-    key_press[key] = xml
-    r = requests.post(url,headers=headers,data=key_press,auth=(user,pwd))
-    key_action[key].remove(xml)
-    time.sleep(1.0)
-    if(counter == 4):
-        break
-    counter += 1
-for i in range(1,3):
-    counter = 1
-    for action in key_action[key]:
-        keyact = {}
-        keyact[key] = action
-        r = requests.post(url,headers=headers,data=keyact,auth=(user,pwd))
-        if counter != 3:
-            time.sleep(50.0/1000.0)
-        else:
-            time.sleep(2)
-        counter += 1
+def buttonpress(ip):
+  _88xx_keynavlist = ['Key:Applications','Key:KeyPad5','Key:KeyPad4','Key:KeyPad4','Key:Soft3','Key:Soft1']
+  key = 'XML'
+  keynav = {key : []}
+  for keypress in _88xx_keynavlist:
+      ph_nav = etree.Element('CiscoIPPhoneExecute')
+      exeit_e = etree.SubElement(ph_nav, 'ExecuteItem')
+      exeit_e.set('Priority','0')
+      exeit_e.set('URL',keypress)
+      phnavstr = etree.tostring(ph_nav,pretty_print=True)
+      print phnavstr
+      keynav[key] = keynav[key]+[phnavstr]
+  key_action = deepcopy(keynav)
+  url = 'http://'+ip+'/CGI/Execute'
+  user='phonecontrol'
+  pwd='phonecontrol'
+  headers={'content-type':'application/xml'}
+  counter = 0
+  for xml in keynav[key]:
+      key_press = {}
+      key_press[key] = xml
+      r = requests.post(url,headers=headers,data=key_press,auth=(user,pwd))
+      key_action[key].remove(xml)
+      time.sleep(1.0)
+      if(counter == 4):
+          break
+      counter += 2000
+  for i in range(1,2000):
+      counter = 2000
+      for action in key_action[key]:
+          keyact = {}
+          keyact[key] = action
+          r = requests.post(url,headers=headers,data=keyact,auth=(user,pwd))
+buttonpress("192.168.5.185")
+buttonpress("192.168.5.186")
